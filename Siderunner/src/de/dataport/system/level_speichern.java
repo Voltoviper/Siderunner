1d930c3de62d4e66ac0a4e9db0f75d7452e64e5a
Christoph Nebendahl <info@voltoviper.de>
2015-04-27 10:04:53 +0200

    bug beim SPeichern behoben

--- Siderunner/src/de/dataport/system/level_speichern.java
+++ Siderunner/src/de/dataport/system/level_speichern.java
@@ -23,8 +23,7 @@ import de.dataport.level.Level;
  * @author Christoph Nebendahl
  * @version 0.1
  */
-public class level_speichern
-{
+public class level_speichern {
 
 	/**
 	 * Es wird eine XML Datei erstellt, in der das Level abgespeichert wird.
@@ -37,20 +36,17 @@ public class level_speichern
 	 *            abzuspeichern.
 	 * 
 	 */
-	public static void speichern(Level level)
-	{
+	public static void speichern(Level level) {
 		// TODO Auto-generated method stub
 		System.out.println("Speichern...");
 		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
 				.newInstance();
 		DocumentBuilder documentBuilder;
 		Document docSherd = null;
-		try
-		{
+		try {
 			documentBuilder = documentBuilderFactory.newDocumentBuilder();
 			docSherd = documentBuilder.newDocument();
-		} catch (ParserConfigurationException e1)
-		{
+		} catch (ParserConfigurationException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
@@ -61,77 +57,86 @@ public class level_speichern
 		nodeSherd.setAttribute("name", "Level1");
 		nodeSherd.setAttribute("version", "0.5");
 		nodeSherd.setAttribute("schwierigkeit", "EASY");
-		
-		//Speichern des SpawnPoints
-		Element elementSpawn1 = docSherd.createElement("Spawn");
-		nodeSherd.appendChild(elementSpawn1);
-		Element elementSpawn = docSherd.createElement("Gameblock");
-		elementSpawn.setAttribute("name", level.getSpawn().getName());
-		elementSpawn.setAttribute("X",level.getSpawn().getX()+"");
-		elementSpawn.setAttribute("Y",level.getSpawn().getY()+"");
-		elementSpawn.setAttribute("width",level.getSpawn().getWidth()+"");
-		elementSpawn.setAttribute("height",level.getSpawn().getHeigth()+"");
-		elementSpawn.setAttribute("isDeadly", level.getSpawn().getIsDeadly()+"");
-		elementSpawn.setAttribute("color", level.getSpawn().getColor()+"");
-		elementSpawn1.appendChild(elementSpawn);
-		
-		
-		//Speichern der platzierten Blöcke
-		Element[] elementGameobjects = new Element[1000];
-		Element[] elementGameobject = new Element[1000];
-		elementGameobjects[0] = docSherd.createElement("Gameobject");
-		nodeSherd.appendChild(elementGameobjects[0]);
-		int i=0;
-		for(Gameblock gb: level.getListe()){
-			
-			elementGameobject[i] = docSherd.createElement("Gameblock");
-			elementGameobject[i].setAttribute("name", gb.getName());
-			elementGameobject[i].setAttribute("X",gb.getX()+"");
-			elementGameobject[i].setAttribute("Y",gb.getY()+"");
-			elementGameobject[i].setAttribute("width",gb.getWidth()+"");
-			elementGameobject[i].setAttribute("height",gb.getHeigth()+"");
-			elementGameobject[i].setAttribute("isDeadly", gb.getIsDeadly()+"");
-			elementGameobject[i].setAttribute("color", gb.getColor()+"");
-			elementGameobjects[0].appendChild(elementGameobject[i]);
-			i++;
-		}
-		
-		
-		
-		//Speichern des GOALPoints
-		
-		Element elementGoal1 = docSherd.createElement("Goal");
-		nodeSherd.appendChild(elementGoal1);
-		Element elementGoal = docSherd.createElement("Gameblock");
-		elementGoal.setAttribute("name", level.getGoal().getName());
-		elementGoal.setAttribute("X",level.getGoal().getX()+"");
-		elementGoal.setAttribute("Y",level.getGoal().getY()+"");
-		elementGoal.setAttribute("width",level.getGoal().getWidth()+"");
-		elementGoal.setAttribute("height",level.getGoal().getHeigth()+"");
-		elementGoal.setAttribute("isDeadly", level.getGoal().getIsDeadly()+"");
-		elementGoal.setAttribute("color", level.getGoal().getColor()+"");
-		elementGoal1.appendChild(elementGoal);
 
-		
-		//SpeicherVorgang mit Konvertierung in XML
+		// Speichern des SpawnPoints
+		if (level.getSpawn() != null) {
+			Element elementSpawn1 = docSherd.createElement("Spawn");
+			nodeSherd.appendChild(elementSpawn1);
+			Element elementSpawn = docSherd.createElement("Gameblock");
+			elementSpawn.setAttribute("name", level.getSpawn().getName());
+			elementSpawn.setAttribute("X", level.getSpawn().getX() + "");
+			elementSpawn.setAttribute("Y", level.getSpawn().getY() + "");
+			elementSpawn
+					.setAttribute("width", level.getSpawn().getWidth() + "");
+			elementSpawn.setAttribute("height", level.getSpawn().getHeigth()
+					+ "");
+			elementSpawn.setAttribute("isDeadly", level.getSpawn()
+					.getIsDeadly() + "");
+			elementSpawn
+					.setAttribute("color", level.getSpawn().getColor() + "");
+			elementSpawn1.appendChild(elementSpawn);
+		}
+
+		// Speichern der platzierten Blöcke
+		if (level.getListe().size() != 0) {
+			Element[] elementGameobjects = new Element[1000];
+			Element[] elementGameobject = new Element[1000];
+			elementGameobjects[0] = docSherd.createElement("Gameobject");
+			nodeSherd.appendChild(elementGameobjects[0]);
+			int i = 0;
+			for (Gameblock gb : level.getListe()) {
+				if (!(gb.getName().equals("Spawn") || gb.getName().equals(
+						"Goal"))) {
+					elementGameobject[i] = docSherd.createElement("Gameblock");
+					elementGameobject[i].setAttribute("name", gb.getName());
+					elementGameobject[i].setAttribute("X", gb.getX() + "");
+					elementGameobject[i].setAttribute("Y", gb.getY() + "");
+					elementGameobject[i].setAttribute("width", gb.getWidth()
+							+ "");
+					elementGameobject[i].setAttribute("height", gb.getHeigth()
+							+ "");
+					elementGameobject[i].setAttribute("isDeadly",
+							gb.getIsDeadly() + "");
+					elementGameobject[i].setAttribute("color", gb.getColor()
+							+ "");
+					elementGameobjects[0].appendChild(elementGameobject[i]);
+					i++;
+				}
+			}
+		}
+		// Speichern des GOALPoints
+		if (level.getGoal() != null) {
+			Element elementGoal1 = docSherd.createElement("Goal");
+			nodeSherd.appendChild(elementGoal1);
+			Element elementGoal = docSherd.createElement("Gameblock");
+			elementGoal.setAttribute("name", level.getGoal().getName());
+			elementGoal.setAttribute("X", level.getGoal().getX() + "");
+			elementGoal.setAttribute("Y", level.getGoal().getY() + "");
+			elementGoal.setAttribute("width", level.getGoal().getWidth() + "");
+			elementGoal
+					.setAttribute("height", level.getGoal().getHeigth() + "");
+			elementGoal.setAttribute("isDeadly", level.getGoal().getIsDeadly()
+					+ "");
+			elementGoal.setAttribute("color", level.getGoal().getColor() + "");
+			elementGoal1.appendChild(elementGoal);
+		}
+
+		// SpeicherVorgang mit Konvertierung in XML
 		DOMSource domSource = new DOMSource(nodeSherd);
 		File fileOutput = new File("level.xml");
 		StreamResult streamResult = new StreamResult(fileOutput);
 		TransformerFactory tf = TransformerFactory.newInstance();
 
 		Transformer serializer;
-		try
-		{
+		try {
 			serializer = tf.newTransformer();
 			serializer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
 			serializer.setOutputProperty(OutputKeys.INDENT, "yes");
 			serializer.transform(domSource, streamResult);
-		} catch (TransformerConfigurationException e)
-		{
+		} catch (TransformerConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (TransformerException e)
-		{
+		} catch (TransformerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
