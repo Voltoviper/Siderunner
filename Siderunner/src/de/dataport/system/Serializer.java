fa638baf29faa8ba99beb4693c8e6279b43246da
Jan <Jan@Notebook-Jan.wak-schulung.local>
2015-06-04 10:34:06 +0200

    spawn&goal placement

--- Siderunner/src/de/dataport/system/Serializer.java
+++ Siderunner/src/de/dataport/system/Serializer.java
@@ -144,8 +144,8 @@ public abstract class Serializer {
 				if (l.getListe().get(0).getX() < 0) {
 					int optimizer = l.getListe().get(0).getX() * -1;
 					l.getListe().forEach(gb -> gb.setX(gb.getX() + optimizer));
-					l.getSpawn().setX(l.getSpawn().getX() + optimizer);
-					l.getGoal().setX(l.getGoal().getX() + optimizer);
+//					l.getSpawn().setX(l.getSpawn().getX() + optimizer);
+//					l.getGoal().setX(l.getGoal().getX() + optimizer);
 				}
 
 	}
