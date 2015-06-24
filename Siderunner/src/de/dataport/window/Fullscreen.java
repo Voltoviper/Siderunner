d3a2131d0a02de23a2fcaf1e14ebffd7bb8de3cc
Jan <Jan@Jan-PC.fritz.box>
2015-06-24 15:49:41 +0200

    bla

--- Siderunner/src/de/dataport/window/Fullscreen.java
+++ Siderunner/src/de/dataport/window/Fullscreen.java
@@ -1,25 +1,19 @@
 package de.dataport.window;
 
 import java.awt.EventQueue;
-import java.awt.Font;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
-import de.dataport.standardcatalog.StandardContent;
 import de.dataport.system.Tastatur;
 
 import javax.swing.JDesktopPane;
 
 import java.awt.Color;
 
-import javax.swing.JButton;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class Fullscreen
 {
